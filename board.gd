13feb323412d2693b57c29a972b8c0b26997c53e
Shuflduf <shuflduf@gmail.com>
2024-08-15 22:10:26 -0600

    placing pieces at certain rows

--- board.gd
+++ board.gd
@@ -1,16 +1,36 @@
 extends Control
 
-@onready var rows: VBoxContainer = $MarginContainer/Rows
+@onready var columns: HBoxContainer = $MarginContainer/Columns
 
-func set_piece(cell_pos: Vector2i):
-	var row = rows.get_child(cell_pos.y)
-	var cell = row.get_child(cell_pos.x)
-	change_color(cell, Color.RED)
+func set_piece(cell_pos: Vector2i, new_colour: Color):
+	var col = columns.get_child(cell_pos.x)
+	var cell = col.get_child(cell_pos.y)
+	#(cell, Color.RED)
 
-func change_color(cell: Panel, new_colour: Color):
 	var panel = cell.get_theme_stylebox("panel").duplicate()
 	panel.bg_color = new_colour
 	cell.add_theme_stylebox_override("panel", panel)
+	cell.name = str(new_colour.to_html(false))
+
+
+#func change_color(cell: Panel, new_colour: Color):
+
 
 func _ready() -> void:
-	set_piece(Vector2i(2, 3))
+	place_piece(1)
+	place_piece(1)
+
+func place_piece(index: int):
+	var cells = columns.get_child(index).get_children()
+	#cells.reverse()
+	#var placed = false
+
+	for i in range(cells.size() - 1, -1, -1):
+		var cell = cells[i]
+		print(cell)
+		if (not cell.name.is_valid_html_color()) or i > cells.size() - 1:
+			#print(cell.name.is_valid_html_color())
+			#print(i > columns.get_child_count() - 3)
+			set_piece(Vector2i(index, i), Color.RED)
+			#placed = true
+			break
