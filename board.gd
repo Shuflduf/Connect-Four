7ebb02d59366835e294f7abee19bbf6ce9a95296
Shuflduf <shuflduf@gmail.com>
2024-08-16 12:05:32 -0600

    tons of helper functions

--- board.gd
+++ board.gd
@@ -4,6 +4,9 @@ extends Control
 
 var player_1_turn = true
 
+var player_1_col = Color.RED
+var player_2_col = Color.BLUE
+
 func set_piece(cell_pos: Vector2i, new_colour: Color):
 	var col = columns.get_child(cell_pos.x)
 	var cell = col.get_child(cell_pos.y)
@@ -15,13 +18,9 @@ func set_piece(cell_pos: Vector2i, new_colour: Color):
 	cell.name = str(new_colour.to_html(false))
 
 
-#func change_color(cell: Panel, new_colour: Color):
-
-
 func _ready() -> void:
 	for col in columns.get_children():
 		col.gui_input.connect(func(event: InputEvent):
-			print("FBHDGFH")
 			if event is InputEventMouseButton:
 				if event.pressed:
 					if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
@@ -41,22 +40,42 @@ func piece_placed(piece: Panel) -> bool:
 	elif piece.name.length() > 6:
 		if piece.name.erase(6, 1).is_valid_html_color():
 			is_color = true
+	get_player(piece)
 	return is_color
+
+func get_player(piece: Panel) -> int:
+	var colour: Color
+	if piece.name.is_valid_html_color():
+		colour = Color(piece.name)
+	elif piece.name.length() > 6:
+		if piece.name.erase(6, 1).is_valid_html_color():
+			colour = Color(piece.name.erase(6, 1))
+	
+	match colour:
+		player_1_col:
+			return 0
+		player_2_col:
+			return 1
+	
+
+func get_panel(pos: Vector2i) -> Panel:
+	var col = columns.get_child(pos.x)
+	var cell = col.get_child(pos.y)
+	return cell
 
 
 func place_piece(index: int):
-	player_1_turn = !player_1_turn
+	
 	var cells = columns.get_child(index).get_children()
 	for i in range(cells.size() - 1, -1, -1):
 		var cell = cells[i]
-		print(cell)
 		
 		if !piece_placed(cell) or i > cells.size() - 1:
-			var piece_col = Color.RED if player_1_turn else Color.BLUE
+			var piece_col = player_1_col if player_1_turn else player_2_col
 			set_piece(Vector2i(index, i), piece_col)
 			break
+	player_1_turn = !player_1_turn
 
 
-func _on_v_box_container_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		print("COL 1 CLICK")
+func win_horizontally(input: Vector2i) -> bool:
+	return false
