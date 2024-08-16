69bf3f6180af9d236a620628d9451da7084b9b5b
Shuflduf <shuflduf@gmail.com>
2024-08-16 10:36:27 -0600

    alternating colours

--- board.gd
+++ board.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var columns: HBoxContainer = $MarginContainer/Columns
 
+var player_1_turn = true
+
 func set_piece(cell_pos: Vector2i, new_colour: Color):
 	var col = columns.get_child(cell_pos.x)
 	var cell = col.get_child(cell_pos.y)
@@ -18,14 +20,12 @@ func set_piece(cell_pos: Vector2i, new_colour: Color):
 
 func _ready() -> void:
 	for col in columns.get_children():
-		for cell in col.get_children():
-			cell.gui_input.connect(func(event: InputEvent):
-				print("FBHDGFH")
-				if event is InputEventMouseButton:
-					if event.pressed:
-						if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
-							print(cell)
-							place_piece(col.get_index()))
+		col.gui_input.connect(func(event: InputEvent):
+			print("FBHDGFH")
+			if event is InputEventMouseButton:
+				if event.pressed:
+					if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+						place_piece(col.get_index()))
 
 func col_full(index):
 	if columns.get_child(index).get_child(0).name.is_valid_html_color():
@@ -35,12 +35,14 @@ func col_full(index):
 func place_piece(index: int):
 	if col_full(index):
 		return
+	player_1_turn = !player_1_turn
 	var cells = columns.get_child(index).get_children()
 	for i in range(cells.size() - 1, -1, -1):
 		var cell = cells[i]
 		print(cell)
 		if (not cell.name.is_valid_html_color()) or i > cells.size() - 1:
-			set_piece(Vector2i(index, i), Color.RED)
+			var piece_col = Color.RED if player_1_turn else Color.BLUE
+			set_piece(Vector2i(index, i), piece_col)
 			break
 
 
