001ae115e65a0c2460f37593c603358d7d096e7d
Shuflduf <shuflduf@gmail.com>
2024-08-16 13:01:58 -0600

    changed player detection and started win

--- board.gd
+++ board.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var columns: HBoxContainer = $MarginContainer/Columns
 
 var player_1_turn = true
+var turn = 0
 
 var player_1_col = Color.RED
 var player_2_col = Color.BLUE
@@ -15,7 +16,7 @@ func set_piece(cell_pos: Vector2i, new_colour: Color):
 	var panel = cell.get_theme_stylebox("panel").duplicate()
 	panel.bg_color = new_colour
 	cell.add_theme_stylebox_override("panel", panel)
-	cell.name = str(new_colour.to_html(false))
+	cell.name = str(new_colour.to_html(false) + "C")
 
 
 func _ready() -> void:
@@ -28,36 +29,32 @@ func _ready() -> void:
 							return
 						place_piece(col.get_index()))
 
+
+
+
 func col_full(index):
-	if piece_placed(columns.get_child(index).get_child(0)):
+	if piece_is_placed(columns.get_child(index).get_child(0)):
 		return true
 	return false
 
-func piece_placed(piece: Panel) -> bool:
+func piece_is_placed(piece: Panel) -> bool:
 	var is_color = false
-	if piece.name.is_valid_html_color():
+	if piece.name.get_slice("C", 0).is_valid_html_color():
 		is_color = true
-	elif piece.name.length() > 6:
-		if piece.name.erase(6, 1).is_valid_html_color():
-			is_color = true
-	get_player(piece)
 	return is_color
 
 func get_player(piece: Panel) -> int:
 	var colour: Color
-	if piece.name.is_valid_html_color():
-		colour = Color(piece.name)
-	elif piece.name.length() > 6:
-		if piece.name.erase(6, 1).is_valid_html_color():
-			colour = Color(piece.name.erase(6, 1))
-	
+
+	colour = Color(piece.name.get_slice("C", 0))
+
 	match colour:
 		player_1_col:
 			return 0
 		player_2_col:
 			return 1
 	return -1
-	
+
 
 func get_panel(pos: Vector2i) -> Panel:
 	var col = columns.get_child(pos.x)
@@ -66,17 +63,23 @@ func get_panel(pos: Vector2i) -> Panel:
 
 
 func place_piece(index: int):
-	
 	var cells = columns.get_child(index).get_children()
 	for i in range(cells.size() - 1, -1, -1):
 		var cell = cells[i]
-		
-		if !piece_placed(cell) or i > cells.size() - 1:
+
+		if !piece_is_placed(cell) or i > cells.size() - 1:
 			var piece_col = player_1_col if player_1_turn else player_2_col
 			set_piece(Vector2i(index, i), piece_col)
+			win_horizontally(Vector2(index, i))
 			break
 	player_1_turn = !player_1_turn
+	turn = int(player_1_turn)
 
 
 func win_horizontally(input: Vector2i) -> bool:
+	var player = get_player(get_panel(input))
+	var first_piece = input
+	while get_player(get_panel(first_piece)) != player:
+		first_piece.x -= 1
+	print(first_piece)
 	return false
