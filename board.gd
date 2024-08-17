09454601be6dee2203c587c9523ebece36850225
Shuflduf <shuflduf@gmail.com>
2024-08-16 20:00:32 -0600

    tween colour bg

--- board.gd
+++ board.gd
@@ -58,8 +58,8 @@ func update_bg_color():
 			new_col = player_1_col
 		1:
 			new_col = player_2_col
-
-	$Colour.modulate = new_col
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property($Colour, "modulate", new_col, 0.3)
 
 
 
